68e448d3a6f1ba39932882a58a84e620bfb88583
Penguin <penguinguy2550@gmail.com>
2025-10-05 22:41:08 +0300

    added by letter county guessing with letter B
    (not a finished feature, the letter C wont work . more letters will be added in the future)

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ counties = {"alba", "arad", "arges", "bacau", "bihor", "bistrita-nasaud", "botos
 
 def all_counties():
     game_counties = set()
-    print("All right, start naming as many counties as you can! Type f at any point to give up, type s to see already guessed counties.")
+    print("All right, start naming as many counties as you can! Type f at any point to give up, type s to see currently guessed counties.")
     while True:
         print("Guess a county:")
         county = input("> ")
@@ -71,7 +71,63 @@ def all_counties():
 # b counties
 
 b_counties = {"bacau", "bihor", "bistrita-nasaud", "botosani", "brasov", "braila", "buzau"}
+
+def b_counties_func():
+    game_counties = set()
+    print("How many Romanian counties starting with the letter B can you name? Type f at any point to give up, type s to see currently guessed counties.")
+    while True:
+        print("Guess a county:")
+        county = input("> ")
+
+
+        # if the country guess is correct
+        if county.lower() in b_counties and not county.lower() in game_counties:
+            game_counties.add(county.lower())
+            print(f"County guessed! {len(game_counties)}/{len(b_counties)}")
+            if len(game_counties) == 7:
+                print(f"You have won! All Romanian counties starting with the letter B have been correctly guessed, {len(game_counties)}/{len(b_counties)}!\nHere's a list of all the counties:\n{game_counties}\nBack to the menu . . .\n\n")
+                counties_by_letter()
+                return
             
+
+        # currently guessed counties
+        elif county.lower() == "s":
+            if len(game_counties) == 0:
+                print(f"No currently guessed counties yet! Type x to exit")
+                while True:
+                    show_exit = input("> ")
+                    if show_exit.lower() == "x":
+                        break
+                    else:
+                        print("That's not a command!")
+
+            else:
+                print(f"Currently guessed counties:\n{game_counties}\nType x to exit")
+                while True:
+                    show_exit = input("> ")
+                    if show_exit.lower() == "x":
+                        break
+                    else:
+                        print("That's not a command!")
+
+        
+
+        # player gives up o7
+        elif county.lower() == "f":
+            print(f"You gave up. You guessed {len(game_counties)}/{len(b_counties)} counties.\nHere are the counties you didn't guess:\n{b_counties.difference(game_counties)}\n\n")
+            counties_by_letter()
+            return
+        
+
+        # county doesnt exist
+        elif county.lower() not in b_counties:
+            print("That's not a county!")
+
+
+        # county already guessed
+        else:
+            print("You have already guessed that county!")
+
 
 
 # main counties by letter menu
@@ -83,10 +139,10 @@ def counties_by_letter():
             menu()
             return
         elif letter == "1":
-            b_counties()
+            b_counties_func()
             return
         elif letter == "2":
-            c_counties()
+            c_counties_func()
             return
         else:
             print("That's not an available option!")
