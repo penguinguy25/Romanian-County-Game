ab7c270c9d0c00e5a4a74ecf5eadb881912344f9
Penguin <penguinguy2550@gmail.com>
2025-10-09 13:29:51 +0300

    unfinished usa sttates
    uh

---
+++ usa.py
@@ -0,0 +1,21 @@
+# usa states guessing
+#
+# main.py usa file
+
+
+usa_states = {"alabama", "alaska", "arizona", "arkansas",
+              "california", "colorado", "connecticut", "delaware",
+              "florida", "georgia", "hawaii", "idaho", "illinois",
+              "indiana", "iowa", "kansas", "kentucky", "louisiana",
+              "maine", "maryland", "massachusetts", "michigan",
+              "minnesota", "mississippi", "missouri", "montana",
+              "nebraska", "nevada", "new hampshire", "new jersey",
+              "new mexico", "new york", "north carolina", "north dakota",
+              "ohio", "oklahoma", "oregon", "pennsylvania", "rhode island",
+              "south carolina", "south dakota", "tennessee", "texas",
+              "utah", "vermont", "virginia", "washington", "west virginia",
+              "wisconsin", "wyoming"}
+
+
+def usa_menu(): 
+    print("USA states main menu, press x to quit")
